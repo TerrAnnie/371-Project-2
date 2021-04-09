aca729bb43862b90e0129e459abf0aadd44b5f20
anodyne99 <60171134+anodyne99@users.noreply.github.com>
2021-04-08 21:52:24 -0500

    Update Project 371.sql
    Code simplification and typo fixing

--- Project 371.sql
+++ Project 371.sql
@@ -6,55 +6,52 @@ DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Moderators;
 DROP TABLE IF EXISTS Advertisements;
 
-
 CREATE TABLE IF NOT EXISTS Categories
-  (Category_ID varchar(3) not null,
-   CatName varchar(20) not null,
-   constraint pk_categories primary key (Category_ID)
-   
+(
+Category_ID varchar(3) not null,
+CatName varchar(20) not null,
+Primary Key (Category_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Statuses
-  (Status_ID varchar(2) not null,
-   StatusName varchar(14) not null,
-   constraint pk_statuses primary key (Status_ID)
+(
+Status_ID varchar(2) not null,
+StatusName varchar(14) not null,
+Primary Key (Status_ID)
 );
 
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS Users
+(
 User_ID varchar(20),
 UserFirst_Name varchar(30) NOT NULL,
 UserLast_Name varchar (30) NOT NULL,
-constraint PK_Users Primary Key (User_ID)
-
-	
+Primary Key (User_ID)
 );
 
-CREATE TABLE IF NOT EXISTS Moderators 
-(
-User_ID varchar(20),
-constraint PK_Moderators Primary Key (User_ID),
-Foreign Key (User_ID) references Users
-ON Delete Restrict
-
+CREATE TABLE IF NOT EXISTS Moderators
+(User_ID varchar(20),
+Primary Key (User_ID),
+Foreign Key (User_ID) references Users(User_ID)
 );
+
 CREATE TABLE IF NOT EXISTS Advertisements
- (
-    Advertisement_ID int(10),
-    AdvTitle varchar(100) not null,
-    AdvDetails varchar(100),
-    AdvDateTime DATE not null,
-    Price FLOAT not null,
+(
+Advertisement_ID int(10),
+AdvTitle varchar(100) not null,
+AdvDetails varchar(100),
+AdvDateTime DATE not null,
+Price FLOAT not null,
+Category_ID varchar(3),
+User_ID varchar(20),
+Moderator_ID varchar(20),
+Status_ID varchar(2),
 
-    Category_ID varchar(3) REFERENCES Categories(Category_ID)
-    On delete restrict,
-    User_ID varchar(10) REFERENCES Users(User_ID)
-    On delete Cascade,
-    Moderator_ID varchar(10) REFERENCES Moderators(Moderator_ID)
-    On delete Set Null,
-	Status_ID varchar(2) REFERENCES Statuses(Status_ID)
-    On delete restrict,
-    constraint pk_advertisements primary key (Advertisement_ID)
- );
+Foreign Key (Category_ID) REFERENCES Categories(Category_ID) ON DELETE RESTRICT,
+Foreign Key (User_ID) REFERENCES Users(User_ID) ON DELETE CASCADE,
+Foreign Key (Moderator_ID) REFERENCES Moderators(User_ID) ON DELETE SET NULL,
+Foreign Key (Status_ID) REFERENCES Statuses(Status_ID) ON DELETE RESTRICT,
+Primary Key (Advertisement_ID)
+);
 
 Insert Into Users (User_ID,UserFirst_Name, UserLast_Name) 
 Values  ('Jsmith', 'John', 'Smith'),
@@ -67,8 +64,8 @@ Insert Into Moderators Values
 ('Ajackson');
 
 
-INSERT INTO Categories 
-Values ('CAT', 'Cars and Trucks'),
+INSERT INTO Categories Values
+('CAT', 'Cars and Trucks'),
 ('HOU', 'Housing'),
 ('ELC', "Electronics"),
 ('CCA', "Child Care");
@@ -83,11 +80,4 @@ INSERT INTO Advertisements Values
 ( 2, 'Nice office desk', 'Nice office desk for sale', '2017-02-15', 50.25, 'HOU', 'Rkale', 'Jsmith', 'AC'),
 ( 3, 'Smart LG TV for $200 ONLY', 'Smart LG TV 52 inches! Really cheap!', '2017-03-15', 200, 'ELC', 'Sali', 'Jsmith', 'AC'),
 ( 4, 'HD Tablet for $25 only', 'Amazon Fire Tablet HD', '2017-03-20', 25, 'CAT', 'Rkale', Null, 'PN'),
-( 5, 'Laptop for $100', 'Amazing HP laptop for $100', '2017-03-20', 100, 'CAT', 'Rkale', Null, 'P');
-
-
-select *
-From Advertisements;
-
-select *
-From Users;
+( 5, 'Laptop for $100', 'Amazing HP laptop for $100', '2017-03-20', 100, 'CAT', 'Rkale', Null, 'PN');
